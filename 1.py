e9b26eca5ad00a5a7568cd25bf744de27c2a2af9
Gourab-coder <gourabdas034@gmail.com>
2026-06-30 13:08:04 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,25 @@
+# variables
+# data
+name = "Gourab Das"
+age = 21
+height = 5.3
+ismale = True
+
+# print
+print(name)
+print(age)
+print(height)
+print("male :", ismale)
+
+# types of variables
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(ismale))
+
+# auto bool ans
+print(5>2)
+
+# input
+collage = input("Collage name : ")
+print(name, "Your collage name is", collage)
